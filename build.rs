7214658d8add36f881686044eb71b4eb62bd06f9
Kang Seonghoon <public+git@mearie.org>
2026-07-21 09:34:43 +0900

    uniform: Set the main thread stack size to 8 MiB in Windows.

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "windows" {
+        println!("cargo:rustc-link-arg=/STACK:8388608");
+    }
+
     #[cfg(feature = "editor")]
     embed_resource::compile("app.rc", embed_resource::NONE)
         .manifest_optional()
